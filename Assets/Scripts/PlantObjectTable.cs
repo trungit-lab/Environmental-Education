afc0a9e8556ec6a0138f92df390ad12f88463373
pltrieuvy-jpg <pltrieuvy@gmail.com>
2025-08-17 15:23:39 +0700

    add new file
    add new file to demo push github

---
+++ Assets/Scripts/PlantObjectTable.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlantObjectTable", menuName = "Scriptable Objects/PlantObjectTable")]
+public class PlantObjectTable : ScriptableObject
+{
+    
+}
